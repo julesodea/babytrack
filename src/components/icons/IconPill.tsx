4df6b37251d4dc141e372e31711fca5a9c4b9305
julesodea <jules.odea@gmail.com>
2026-02-13 12:17:47 +1300

    Refactor IconPill component for improved design
    - Updated the SVG path in IconPill to enhance visual representation.
    - Simplified the icon structure for better clarity and consistency in the UI.

--- src/components/icons/IconPill.tsx
+++ src/components/icons/IconPill.tsx
@@ -14,7 +14,7 @@ export function IconPill({ className }: IconProps) {
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeWidth={1.5}
-        d="M8 6L16 6C17.1046 6 18 6.89543 18 8L18 16C18 17.1046 17.1046 18 16 18L8 18C6.89543 18 6 17.1046 6 16L6 8C6 6.89543 6.89543 6 8 6ZM6 12L18 12M12 6L12 9"
+        d="M7 7H17V17H7V7ZM12 9V15M9 12H15"
       />
     </svg>
   );
